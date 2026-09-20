0815ac2462cd0cc8d323772142b12914df8999f1
Adolph <adolphusmulanga723@gmail.com>
2026-09-20 18:55:25 +0200

    Fix Vercel compatibility

--- api/adminChangePassword.js
+++ api/adminChangePassword.js
@@ -14,7 +14,7 @@ if (!admin.apps.length) {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS setup
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
